a2cfca2564ee4287cd0b3e795c24fa6ef122cdef
jeckaGoonz <tatuajes.jessica.g@gmail.com>
2024-06-17 21:17:52 -0400

    proyecto inicial
    imprimir codigo en pantalla

---
+++ jess.py
@@ -0,0 +1,16 @@
+# mi_Lista = []
+# nuevaLista =[]
+# input ("ingresar numeros a la lista indicada")
+# mi_Lista = mi_Lista.split()
+# print(mi_Lista)
+# def separar_numeros_inpares(mi_Lista):
+#     numeros_inpares=[]
+#     numeros_pares=[]
+#     print(nuevaLista)
+#     for i in nuevaLista:
+17%2
+
+
+
+
+ 
